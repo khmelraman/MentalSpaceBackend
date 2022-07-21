9c0b5749e4af2ced6e429cb89744b01e814a112a
khmelraman <khmelraman3922@students.esuhsd.org>
2022-07-21 11:17:58 -0700

    fix AssignmentTypeRowMapper

--- src/main/java/dev/mentalspace/wafflecone/assignmentType/AssignmentTypeRowMapper.java
+++ src/main/java/dev/mentalspace/wafflecone/assignmentType/AssignmentTypeRowMapper.java
@@ -8,9 +8,9 @@ public class AssignmentTypeRowMapper implements RowMapper<AssignmentType> {
     @Override
     public AssignmentType mapRow(ResultSet row, int rowNum) throws SQLException {
         AssignmentType assignmentEntryShortcut = new AssignmentType();
-        assignmentEntryShortcut.assignmentEntryShortcutId   = row.getLong  ("assignment_type_id");
-        assignmentEntryShortcut.teacherId                   = row.getLong  ("teacher_id");
-        assignmentEntryShortcut.value                       = row.getString("value");
+        assignmentEntryShortcut.assignmentTypeId    = row.getLong  ("assignment_type_id");
+        assignmentEntryShortcut.teacherId           = row.getLong  ("teacher_id");
+        assignmentEntryShortcut.value               = row.getString("value");
         return assignmentEntryShortcut;
     }
 }
